66e36ec82264a5ffba34e3730f9d353b7052f96f
Andrew Dinh <adinh@sightmachine.com>
2019-04-23 18:03:15 -0700

    Revert App and create separate file for first post

---
+++ react-animations/src/FirstPost.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Spring } from "react-spring/renderprops";
+import "./App.css";
+
+function FirstPost() {
+  return (
+    <Spring
+      from={{ opacity: 0.6, marginTop: -50 }}
+      to={{ opacity: 1, marginTop: 50 }}
+    >
+      {props => (
+        <div style={props} className="App">
+          <article className="post">
+            <h1>My first posts</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate rerum reprehenderit consectetur porro similique
+              reiciendis ex consequuntur tempore! Similique, pariatur
+              harum.Facilis, accusantium quam labore incidunt soluta
+              suscipit ipsa omnis.
+            </p>
+          </article>
+        </div>
+      )}
+    </Spring>
+  );
+}
+
+export default FirstPost;
