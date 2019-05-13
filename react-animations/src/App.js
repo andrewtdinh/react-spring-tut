d3ee5c54e1c4d738486ffe6e4127a0d42307b346
Andrew Dinh <adinh@sightmachine.com>
2019-05-12 23:28:40 -0700

    Supply items to DragList

--- react-animations/src/App.js
+++ react-animations/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <DragList />
+        <DragList items={ 'Lorem ipsum dolor sit'.split(' ') } />/>
       </header>
     </div>
   );
