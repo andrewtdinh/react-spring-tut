d35337a4619c6d71e1240adea4fad7a918256df0
Andrew Dinh <adinh@sightmachine.com>
2019-05-19 00:30:00 -0700

    Import CardStack.css into CardStack component

--- react-animations/src/CardStack.jsx
+++ react-animations/src/CardStack.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { useSprings, animated, interpolate } from "react-spring";
 import { useGesture } from "react-use-gesture";
-import "./styles.css";
+import "./CardStack.css";
 
 const cards = [
   "https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg",
