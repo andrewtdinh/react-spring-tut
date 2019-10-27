36633237ddd7c03f1fa38b711150fba54b1b33e9
Andrew Dinh <adinh@sightmachine.com>
2019-10-26 19:39:09 -0700

    Create a Toggle component

---
+++ levelup-react-spring/src/Toggle.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Toggle = () => {
+  const [ isToggled, setToggle ] = useState(false);
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+    </div>
+  )
+}
+
+export default Toggle;
