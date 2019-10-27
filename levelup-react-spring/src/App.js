e3205b77e5aad54bf04079b70b9a6db01a3a0f26
Andrew Dinh <adinh@sightmachine.com>
2019-10-26 19:31:25 -0700

    Do not need a to object

--- levelup-react-spring/src/App.js
+++ levelup-react-spring/src/App.js
@@ -8,15 +8,13 @@ const App = () => {
     from: {
       opacity: 0
     },
-    to: {
-      opacity: 1
-    }
+    opacity: 1
   })
 
   return (
     <animated.div className="App" style={fade} >
       <header className="App-header">
-        <img src={logo} className="logo" />
+        <img src={logo} className="logo" alt="logo" />
         <button className="menu-button">Menu</button>
       </header>
     </animated.div>
