1f972730e541bccdc71452021e226a4bb7f0b832
Andrew Dinh <adinh@sightmachine.com>
2019-10-31 21:33:33 -0700

    Fix bug in defining spring hooks

--- levelup-react-spring/src/Blob.js
+++ levelup-react-spring/src/Blob.js
@@ -2,16 +2,16 @@ import React, { useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 
 export default function Goo() {
-  const [{ pos1 }, set] = useSpring({ pos1: [0, 0], config: fast });
-  const [{ pos2 }] = useSpring({ pos2: pos1, config: slow });
-  const [{ pos3 }] = useSpring({ pos3: pos2, config: slow });
+  const [{ pos1 }, set] = useSpring({ pos1: [0, 0], config: 'fast' });
+  const [{ pos2 }] = useSpring({ pos2: pos1, config: 'slow' });
+  const [{ pos3 }] = useSpring({ pos3: pos2, config: 'slow' });
   const trans = (x, y) => `translate3d(${x}px, ${y}px, 0) translate3d(-50%, -50%, 0)`
 
-  useEffect = () => {
+  useEffect(() => {
     const handler = ({ clientX, clientY }) => set({ pos1: [clientX, clientY] });
     window.addEventListener('mousemove', handler);
     return () => window.removeEventListener('mousemove', handler);
-  }
+  }, []);
 
   return (
     <>
