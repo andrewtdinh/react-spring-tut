177acf3837c30812287eb920b1f88c51be9a49ee
Andrew Dinh <adinh@sightmachine.com>
2019-10-31 20:52:48 -0700

    Add useEffect to Blob to add an event listener when user moves mouse

--- levelup-react-spring/src/Blob.js
+++ levelup-react-spring/src/Blob.js
@@ -6,4 +6,10 @@ export default function Goo() {
   const [{ pos2 }] = useSpring({ pos2: pos1, config: slow });
   const [{ pos3 }] = useSpring({ pos3: pos2, config: slow });
   const trans = (x, y) => `translate3d(${x}px, ${y}px, 0) translate3d(-50%, -50%, 0)`
+
+  useEffect = () => {
+    const handler = ({ clientX, clientY }) => set({ pos1: [clientX, clientY] });
+    window.addEventListener('mousemove', handler);
+    return () => window.removeEventListener('mousemove', handler);
+  }
 }
