8019fd2af640b034af87d3739b06f8e0123b3d90
Andrew Dinh <adinh@sightmachine.com>
2019-10-31 20:39:54 -0700

    define some spring hooks and transform function

--- levelup-react-spring/src/Blob.js
+++ levelup-react-spring/src/Blob.js
@@ -5,4 +5,5 @@ export default function Goo() {
   const [{ pos1 }, set] = useSpring({ pos1: [0, 0], config: fast });
   const [{ pos2 }] = useSpring({ pos2: pos1, config: slow });
   const [{ pos3 }] = useSpring({ pos3: pos2, config: slow });
+  const trans = (x, y) => `translate3d(${x}px, ${y}px, 0) translate3d(-50%, -50%, 0)`
 }
